7177226789abd85784c89d70f2981c7c41db27ad
TomaBurch <toma.burchuladze.1@btu.edu.ge>
2022-12-01 08:57:20 -0800

    first commit

---
+++ app/src/main/java/com/example/fragments/fragments/PictureFragment.kt
@@ -0,0 +1,17 @@
+package com.example.fragments.fragments
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.fragments.R
+
+class PictureFragment : Fragment(R.layout.fragment_picture) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
+
+
